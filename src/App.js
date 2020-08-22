3c2972dac17e92988497fad4ca6c55a0aeb72f5e
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-08-23 00:18:07 +0500

    deploying web

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [x] = useState(0)
+
+  useEffect(() => {
+    console.log('I will run on every reder')
+  })
+
+  useEffect(() => {
+    console.log('I will run after first reder like (componentDidMount)')
+  }, [])
+
+  useEffect(() => {
+    console.log('I will run when ever the x value change')
+  }, [x])
+
+  useEffect(() => {
+    console.log('statement outside return will run every time if use effect is not has any depende state or null array ')
+    return () => {
+      console.log('hello im useEffect return function will be run on component unmount/end/finishing state')
+    }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
